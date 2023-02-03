e01556c1833476e3313704387d5f455cc1d79d77
Miguel Angel Archila García <miguel.4to.1994@gmail.com>
2023-02-02 21:45:00 -0600

    memorizando conversacion

--- AIWhatsapp.js
+++ AIWhatsapp.js
@@ -8,14 +8,24 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+let conversation = '';
+let countMessage = 0;
+
+function pushConversation(txt){
+    conversation += txt + ' \n';
+    countMessage++;
+}
+
 async function answerMessage(msg) {
 
+    pushConversation(msg);
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Responde a mi novia: " + msg,
-        max_tokens: 4000
+        // prompt: "Responde a mi amigo: " + msg,
+        prompt: countMessage === 1 ? `responde a mi amigo: ${conversation}` : conversation,
+        max_tokens: 100
     });
-
+    pushConversation(completion.data.choices[0].text);
     return completion.data.choices[0].text;
 }
 
@@ -31,17 +41,38 @@ client.on('qr', qr => {
 client.on('ready', () => {
     // console.log('Client is ready!');
     console.log('Inteligencia Artificial para WhatsApp Activada');
+    // client.sendMessage("50251268484@c.us", "Prueba de mensaje de un bot");
+
 });
 
 
 client.on('message', async message => {
     // console.log(message.from);
     // console.log(message.body);
-    if (message.from === "50251831144@c.us") {
-        console.log("Mensaje de mi novia:");
+
+    // if (message.from === "50251831144@c.us") {
+    //     console.log("Mensaje de mi novia:");
+    //     console.log(message.body);
+    //     const mensaje = await answerMessage(message.body);
+    //     // client.sendMessage(message.from, mensaje);
+    //     console.log("RESPUESTA GENERADA POR INTELIGENCIA ARTIFICIAL:");
+    //     console.log(mensaje);
+    // }
+
+    // if (message.from === "50251268484@c.us") {
+    //     console.log("Mensaje de robin:");
+    //     console.log(message.body);
+    //     const mensaje = await answerMessage(message.body);
+    //     client.sendMessage(message.from, mensaje);
+    //     console.log("RESPUESTA GENERADA POR INTELIGENCIA ARTIFICIAL:");
+    //     console.log(mensaje);
+    // }
+
+    if (message.from === "50242345578@c.us") {
+        console.log("Mensaje de oscar:");
         console.log(message.body);
         const mensaje = await answerMessage(message.body);
-        // client.sendMessage(message.from, mensaje);
+        client.sendMessage(message.from, mensaje);
         console.log("RESPUESTA GENERADA POR INTELIGENCIA ARTIFICIAL:");
         console.log(mensaje);
     }
--- chatGptTest.js
+++ chatGptTest.js
@@ -5,6 +5,13 @@ require('dotenv').config()
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+let conversation = '';
+
+function pushConversation(txt){
+    conversation += txt + ' \n';
+}
+
 const openai = new OpenAIApi(configuration);
 const rl = readline.createInterface({
     input: process.stdin,
@@ -13,13 +20,15 @@ const rl = readline.createInterface({
 
 function askQuestion() {
     rl.question("Ingresa tu pregunta: ", async (answer) => {
+        pushConversation(answer);
         const completion = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: answer,
-            max_tokens: 4000
+            prompt: conversation,
+            max_tokens: 100
         });
-        console.log("logitud: ", completion.data.choices[0].text.length);
         console.log(completion.data.choices[0].text);
+        console.log(completion.data);
+        pushConversation(completion.data.choices[0].text);
         askQuestion();
     });
 }
