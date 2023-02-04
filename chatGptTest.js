4ca60606a21aa695882cfccaed3c60a09dfabcd0
Miguel Angel Archila García <miguel.4to.1994@gmail.com>
2023-02-03 23:16:33 -0600

    intentando limpiar los tokens para que no sobrepase el limite son perder el contexto de la conversacion

--- chatGptTest.js
+++ chatGptTest.js
@@ -8,9 +8,20 @@ const configuration = new Configuration({
 
 let conversation = '';
 
+let preTrainingGirlFriend = 'vas ha chatear con mi novia tomando en cuenta lo siguiente: esta enojada conmigo porque no respondo rapido, quiere ir al parque y '+ 
+ 'quiere comer pizza, responde con mensajes cortos no mas de 20 palabras, agrega la palabra amor en cada respuesta, la conversacion comienza a partir del sigiguiente mensaje:';
+
+ pushConversation(preTrainingGirlFriend);
+
 function pushConversation(txt){
     conversation += txt + ' \n';
 }
+
+function clearTokens(){
+    conversation = preTrainingGirlFriend + ' \n' + conversation.substring(preTrainingGirlFriend.length+500);
+}
+
+console.log(conversation);
 
 const openai = new OpenAIApi(configuration);
 const rl = readline.createInterface({
@@ -26,9 +37,16 @@ function askQuestion() {
             prompt: conversation,
             max_tokens: 100
         });
-        console.log(completion.data.choices[0].text);
-        console.log(completion.data);
+        // console.log(completion.data.choices[0].text);
         pushConversation(completion.data.choices[0].text);
+        console.log("Conversacion:");
+        console.log(conversation);
+        console.log("Tokens: " + conversation.length);
+        if (conversation.length > 3000) {
+            console.log('limpiando tokens');
+            clearTokens();
+        }
+        console.log("Tokens: " + conversation.length);
         askQuestion();
     });
 }
