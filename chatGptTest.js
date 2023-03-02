a959822420a3ab15d0acbc8b009acfe223650196
Miguel Angel Archila García <miguel.4to.1994@gmail.com>
2023-03-02 17:37:15 -0600

    realizando pruebas

--- chatGptTest.js
+++ chatGptTest.js
@@ -12,9 +12,24 @@ let preTrainingWords =
 `Respondele a mis clientes tomando en cuenta lo siguiente:
 somos una tienda con multiples productos,
 atendemos 24/7,
+preguntar si lo quiere agregar al carrito, llevar el control del carrito,
+dar el total cada vez que agregue algo al carrito
+preguntar si desea algo mas,
+si ya no desea nada mas responder: orden realizada,
+en la siguinete linea crear un json con los datos del carrito sin ningun otro mensaje solo el json, ejemplo: 
+{
+    "productos": [
+        {
+            "nombre": leche,
+            "precio": 20.00
+        }
+        ...
+    }
+    y asi con todos los productos del carrito,
 tenemos los siguientes productos:
 1. leche a Q20.00
-2. Cereeal a Q15.00
+2. Cereal a Q15.00
+3. harina a Q4.00 la libra
 la conversacion comienza ahora:
 `;
 
